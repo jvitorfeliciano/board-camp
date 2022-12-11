b617df02c20f6d45b982acfa6b6ff8aab2f59a14
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-11 19:46:00 -0300

    feat: add postGame function

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -31,3 +31,21 @@ export const getGames = async (req, res) => {
     return res.status(500).send({ message: err.message });
   }
 };
+
+export const postGame = async (req, res) => {
+  const { name, image, stockTotal, categoryId, pricePerDay } =
+    res.locals.gameInformation;
+
+  try {
+    await connectionDB.query(
+      `INSERT INTO  games 
+            (name, image, "stockTotal", "categoryId", "pricePerDay")
+       VALUES
+            ($1,$2, $3, $4, $5)`,
+      [name, image, stockTotal, categoryId, pricePerDay]
+    );
+    return res.sendStatus(201);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { getGames } from "../controllers/gamesController.js";
+import { getGames, postGame } from "../controllers/gamesController.js";
 import gameValidation from "../middlewares/gameMiddleware.js";
 
 const gamesRouter = Router();
 
 gamesRouter.get("/games", getGames);
-gamesRouter.post("/games", gameValidation);
+gamesRouter.post("/games", gameValidation, postGame);
 export default gamesRouter;
