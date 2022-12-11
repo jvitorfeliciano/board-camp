d6abe29516a03dd7a9c1f2edf39a8a479794efec
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-11 19:25:03 -0300

    feat: add JOIN in the gatGames function

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -6,15 +6,24 @@ export const getGames = async (req, res) => {
   try {
     if (name) {
       const filteredGamesByName = await connectionDB.query(
-        `SELECT * 
+        `SELECT games.* , categories.name AS "categoryName"
          FROM games 
-         WHERE  name ILIKE  $1 || '%'`,
+         WHERE  name ILIKE  $1 || '%'
+         JOIN categories
+         ON games."categoryId" = categories.id
+         `,
         [name]
       );
 
       return res.send(filteredGamesByName.rows);
     } else {
-      const allGames = await connectionDB.query(`SELECT * FROM games`);
+      const allGames = await connectionDB.query(
+        `SELECT games.* , categories.name AS categoryName
+         FROM games 
+         JOIN categories
+         ON games."categoryId" = categories.id
+         `
+      );
 
       return res.send(allGames.rows);
     }
