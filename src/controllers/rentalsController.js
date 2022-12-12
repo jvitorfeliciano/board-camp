d1f5c5e55d72b52c23b68ac35a56fde036633b3d
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-12 00:08:00 -0300

    feat: add getRentals function

--- src/controllers/rentalsController.js
+++ src/controllers/rentalsController.js
@@ -1,5 +1,73 @@
 import connectionDB from "../db/db.js";
 
+export const getRentals = async (req, res) => {
+  const { customerId, gameId } = req.query;
+  const baseQuery = `SELECT
+                          rentals.*, customers.name AS "customerName", games.name AS "gameName", games."categoryId", games."categoryId", categories.name As "categoryName"
+                      FROM rentals  
+                      JOIN customers  
+                      ON rentals."customerId" =  customers.id
+                      JOIN games
+                      ON rentals."gameId" = games.id
+                      JOIN categories
+                      ON games."categoryId"= categories.Id
+  
+  `;
+
+  function query(firstQuery, secondQuery) {
+    if (!firstQuery && !secondQuery) {
+      return baseQuery;
+    } else if (firstQuery) {
+      return baseQuery + `WHERE customers.id = $1`;
+    } else if (secondQuery) {
+      return baseQuery + `WHERE games.id = $1`;
+    }
+  }
+
+  function dataArray(firstQuery, secondQuery) {
+    if (!firstQuery && !secondQuery) {
+      return;
+    } else if (firstQuery) {
+      return [firstQuery];
+    } else if (secondQuery) {
+      return [secondQuery];
+    }
+  }
+
+  try {
+    const rentals = await connectionDB.query(
+      query(customerId, gameId),
+      dataArray(customerId, gameId)
+    );
+    console.log(rentals.rows);
+    const formattedRentals = rentals.rows.map((rentals) => {
+      return {
+        id: rentals.id,
+        customerId: rentals.customerId,
+        gameId: rentals.gameId,
+        rentDate: rentals.rentDate,
+        daysRented: rentals.daysRented,
+        returnDate: rentals.returnDate,
+        originalPrice: rentals.originalPrice,
+        delayFee: rentals.delayFee,
+        customer: {
+          id: rentals.customerId,
+          name: rentals.customerName,
+        },
+        game: {
+          id: rentals.gameId,
+          name: rentals.gameName,
+          categoryId: rentals.categoryId,
+          categoryName: rentals.categoryName,
+        },
+      };
+    });
+    return res.send(formattedRentals);
+  } catch (err) {
+    return res.status(500).send({ message: err.message });
+  }
+};
+
 export const postRental = async (req, res) => {
   const { customerId, gameId, daysRented } = res.locals.rentalInformations;
   const { pricePerDay } = res.locals.gameInformations;
@@ -30,7 +98,6 @@ export const postRental = async (req, res) => {
     );
     return res.sendStatus(201);
   } catch (err) {
-    return res.status(500).send({ message: "Server error" });
+    return res.status(500).send({ message: err.message });
   }
 };
-
--- src/routes/rentalsRouter.js
+++ src/routes/rentalsRouter.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
-import { postRental } from "../controllers/rentalsController.js";
-import { rentalConditionsValidation, rentalSchemaValidation } from "../middlewares/rentalsMiddleware.js";
+import { getRentals, postRental } from "../controllers/rentalsController.js";
+import {
+  rentalConditionsValidation,
+  rentalSchemaValidation,
+} from "../middlewares/rentalsMiddleware.js";
 
 const rentalsRouter = Router();
 
-rentalsRouter.post("/rentals", rentalSchemaValidation, rentalConditionsValidation, postRental);
+rentalsRouter.get("/rentals",getRentals);
+rentalsRouter.post(
+  "/rentals",
+  rentalSchemaValidation,
+  rentalConditionsValidation,
+  postRental
+);
 
 export default rentalsRouter;
