e392355846538d219f8f7e30f8a7dee79f2724c0
João Vitor Feliciano <jvitorfeliciano1@gmail.com>
2022-12-11 21:05:00 -0300

    feat add getCustomers function

---
+++ src/controllers/customersController.js
@@ -0,0 +1,24 @@
+import connectionDB from "../db/db.js";
+
+export const getCustomers = async (req, res) => {
+  const { cpf } = req.query;
+
+  try {
+    if (cpf) {
+      const customersFilteredByCpf = await connectionDB.query(
+        `SELECT *
+         FROM customers
+         WHERE cpf LIKE $1 ||'%'
+        `,
+        [cpf]
+      );
+      res.send(customersFilteredByCpf.rows);
+    } else {
+      const allCustomers = await connectionDB.query(`SELECT * FROM CUSTOMERS`);
+      res.send(allCustomers.rows);
+    }
+  } catch (err) {
+    return res.status(500).send({ message: "Server error" });
+  }
+};
+
